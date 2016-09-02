8e51e7b374ba90e38a75e83970a58922545cbf4d
Tran Van Tai <millionairetai@gmail.com>
2016-09-02 20:19:04 +0700

    # add task dropdown

---
+++ member/web/app/controllers/notifyCtrl.js
@@ -0,0 +1,5 @@
+//list auhthorities
+appRoot.controller('NotifyCtrl', ['$scope', 'notifyService', '$rootScope', 'PER_PAGE', 'MAX_PAGE_SIZE','$sce',
+    function ($scope, notifyService, $rootScope, PER_PAGE, MAX_PAGE_SIZE, $sce) {
+
+    }]);
