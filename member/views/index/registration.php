7eeed02b194b65d236e0aa3348d59dc54590a5ce
Tran Van Tai <millionairetai@gmail.com>
2016-09-27 14:01:43 +0700

    # merge task from NhatNew - employee list screen

--- member/views/index/registration.php
+++ member/views/index/registration.php
@@ -2,9 +2,48 @@
 
 use yii\widgets\ActiveForm;
 ?>
-<div class="login-box">
-
-    <div class="login-box-body">
-      REGISTRATION HERE
-    </div><!-- /.login-box-body -->
-</div><!-- /.login-box -->
+<div class="container">
+    <div class="row">
+        <div class="register-box">
+            <form role="form">
+                <h2>Confirming registration on iofficez</h2>
+                <hr/>
+                <p><b>Email</b>: hoang_tuan@gmail.com</p>
+                <div class="row">
+                    <div class="col-xs-6 col-sm-6 col-md-6">
+                        <div class="form-group">
+                            <input type="text" name="first_name" id="first_name" class="form-control" placeholder="First Name" tabindex="1">
+                        </div>
+                    </div>
+                    <div class="col-xs-6 col-sm-6 col-md-6">
+                        <div class="form-group">
+                            <input type="text" name="last_name" id="last_name" class="form-control" placeholder="Last Name" tabindex="2">
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <input type="text" name="phone_number" id="phone_number" class="form-control" placeholder="Password" tabindex="3">
+                </div>
+                <div class="form-group">
+                    <input type="email" name="email" id="email" class="form-control" placeholder="Re-password" tabindex="4">
+                </div>
+                <hr/>
+                <div class="row">
+                    <div class="col-xs-8">
+                        <div class="checkbox icheck">
+                            <label class="">
+                                <div class="icheckbox_square-blue" aria-checked="false" aria-disabled="false" style="position: relative;"><input type="checkbox" style="position: absolute; top: -20%; left: -20%; display: block; width: 140%; height: 140%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"><ins class="iCheck-helper" style="position: absolute; top: -20%; left: -20%; display: block; width: 140%; height: 140%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"></ins></div> I agree to the <a href="#">terms</a>
+                            </label>
+                        </div>
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-xs-4">
+                        <button type="submit" class="btn btn-primary btn-block btn-flat">Register</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <a href="login" class="text-center">I already have a membership</a>
+            </form>
+        </div>
+    </div>
+</div>
