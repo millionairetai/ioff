30e88623ab2ab6b08726c326b6725a3d05e8e2a2
nayoka akira <nguyenvannhat080890@gmail.com>
2016-10-04 21:42:29 +0700

    fix eventfixed.js directives

--- member/web/app/directives/eventFixed.js
+++ member/web/app/directives/eventFixed.js
@@ -6,11 +6,12 @@ appRoot.directive('eventFixed', function ($window) {
         restrict: 'A',
         link: function (scope, element, attrs) {
             var topClass = 'sidebarfixed', // get CSS class
-                    offsetTop = element.offset().top; // get element's top relative to the document
+                    offsetTop = element.offset().top, // get element's top relative to the document
+                    offsetparentTop = element.parent().offset().top;
             $win.on('scroll', function (e) {
                 if ($win.scrollTop() >= offsetTop) {
                     element.addClass(topClass);
-                    element.css('top', $win.scrollTop() - 50);
+                    element.css('top', $win.scrollTop() - offsetparentTop + 70);
                 } else {
                     element.removeClass(topClass);
                     element.css('top', '0');
