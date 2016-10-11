e3c9b176608bb968c7bb2754f9a58c4455065249
nayoka akira <nguyenvannhat080890@gmail.com>
2016-10-11 16:05:49 +0700

    email&password

--- member/views/index/forget_password.php
+++ member/views/index/forget_password.php
@@ -4,8 +4,31 @@ use yii\widgets\ActiveForm;
 ?>
 <div class="container">
     <div class="row">
-        <div class="register-box">
-        FORGET PASSWORD HERE
+        <div class="forget-password">
+            <form class="form-horizontal" role="form">
+                <div class="box box-solid">
+                    <div class="box-header with-border">
+                        <h3>Tìm tài khoản của bạn</h3>
+                        <span>
+                            Chúng tôi có thể giúp bạn đặt lại mật khẩu của mình bằng địa chỉ email được liên kết với tài khoản của bạn.
+                        </span>
+                    </div>
+                    <div class="box-body">
+                        <div class="form-group">
+                            <label for="inputEmail" class="col-xs-2 control-label"><i class="fa fa-envelope"></i></label>
+                            <div class="col-xs-7 no-padding">
+                                <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box-footer">
+                        <div class="pull-right">
+                            <button type="submit" class="btn btn-primary">Tìm kiếm</button>
+                            <button type="submit" class="btn btn-danger">Hủy</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 </div>
--- member/views/index/reset_password.php
+++ member/views/index/reset_password.php
@@ -3,9 +3,32 @@
 use yii\widgets\ActiveForm;
 ?>
 <div class="container">
-    <div class="row">
-        <div class="register-box">
-        Rese password HERE
+    <div class="box reset-password">
+        <div class="box-header with-border">
+            <h3 class="text-center box-title">Thay đổi mật khẩu </h3>
+        </div>
+        <div class="box-body">
+            <div class="col-md-4">
+                <div class="box-profile">
+                    <img class="profile-user-img img-responsive img-circle" src="http://member.centeroffice.dev/flies/employee/profileImageDefault.jpg" alt="User profile picture">
+                    <h3 class="profile-username text-center">Nina Mcintire</h3>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <form class="form-horizontal" role="form">
+                    <div class="form-group">
+                        <input type="password" class="form-control" id="inputNewpassword" placeholder="Mật khẩu mới">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" id="inputRenewPassword" placeholder="Nhập lại mật khẩu mới">
+                    </div>
+                    <div class="form-group">
+                        <div class="pull-right">
+                            <button type="submit" class="btn btn-primary">Thay đổi</button>
+                        </div>
+                    </div>
+                </form><!-- End form-->
+            </div>
         </div>
     </div>
 </div>
