60dc5d89d2e9cf43ae159256cdc43aebcb7c992a
nayoka akira <nguyenvannhat080890@gmail.com>
2016-12-13 14:54:58 +0700

    Change avatar employee: Dialog

--- member/web/app/controllers/employeeCtrl.js
+++ member/web/app/controllers/employeeCtrl.js
@@ -348,7 +348,7 @@ appRoot.controller('profileCtrl', ['$scope', '$rootScope', 'alertify', '$timeout
             var modalInstance = $uibModal.open({
                 templateUrl: 'app/views/employee/changeProfile.html',
                 controller: 'changeProfileCtrl',
-                size: 'lg',
+                size: 'sm',
                 keyboard: true,
                 backdrop: 'static',
                 resolve: {
