9ef8ab3b530c73a441182a0b8d46536d36edf508
Tran Van Tai <millionairetai@gmail.com>
2016-12-16 09:43:03 +0700

    # completed.

--- member/web/app/minify/minify_controller.js
+++ member/web/app/minify/minify_controller.js
@@ -273,7 +273,6 @@ appRoot.controller('authorityDetailCtrl', ['$scope', '$uibModalInstance', 'autho
         };
         
         $scope.getAuthority();
-        
         $scope.cancel = function () {
             $uibModalInstance.dismiss('cancel');
         };
