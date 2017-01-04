38d7af3361e69600db92d3dcbf38ccd88a93ca03
Tran Van Tai <millionairetai@gmail.com>
2017-01-04 14:21:52 +0700

    # merge master branch - 5f73567(payment_history version1)

--- backend/views/invoice/payment_history.php
+++ backend/views/invoice/payment_history.php
@@ -1,285 +1,38 @@
-
+<section class="content-header">
+    <h1>PAYMENT HISTORY</h1>
+</section>
 <section class="content">
-    <div class="row">
-        <div class="col-md-4 col-sm-6 col-xs-12">
-            <div class="info-box">
-                <span class="info-box-icon bg-aqua"><i class="fa fa-building-o"></i></span>
-
-                <div class="info-box-content">
-                    <span class="info-box-text">Company</span>
-                    <span class="info-box-number">23 new company</span>
-                    <span class="progress-description">
-                        13 free, 10 fee
-                    </span>
-                </div>
-                <!-- /.info-box-content -->
+    <ul class="timeline-create">
+        <li class='work'>
+            <div class="relative">
+                <span class='date'>20/8/2015</span>
+                <span class='circle'></span>
             </div>
-            <!-- /.info-box -->
-        </div>
-        <!-- /.col -->
-        <div class="col-md-4 col-sm-6 col-xs-12">
-            <div class="info-box">
-                <span class="info-box-icon bg-green"><i class="fa fa-usd"></i></span>
-
-                <div class="info-box-content">
-                    <span class="info-box-text">Revenue</span>
-                    <span class="info-box-number">198,000,00<small>VND</small></span>
-                </div>
-                <!-- /.info-box-content -->
-            </div>
-            <!-- /.info-box -->
-        </div>
-        <!-- /.col -->
-        <div class="col-md-4 col-sm-6 col-xs-12">
-            <div class="info-box">
-                <span class="info-box-icon bg-yellow"><i class="fa fa-line-chart"></i></span>
-
-                <div class="info-box-content">
-                    <span class="info-box-text">Percent</span>
-                    <span class="info-box-number">43<small>%</small></span>
-                </div>
-                <!-- /.info-box-content -->
-            </div>
-            <!-- /.info-box -->
-        </div>
-        <!-- /.col -->
-    </div>
-    <div class="row">
-        <div class="col-md-8">
-            <!-- Default box -->
-            <div class="box">
-                <div class="box-header with-border">
-                    <h3 class="box-title">Overview system</h3>
-                    <div class="box-tools pull-right">
-                        <button class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse"><i class="fa fa-minus"></i></button>
-                        <button class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove"><i class="fa fa-times"></i></button>
-                    </div>
-                </div>
-                <div class="box-body">
-                    <form role="form" class="form-horizontal">
-                        <div class="form-group">
-                            <p class="col-sm-8 help-block text-right">(Contains of today, week, month, year)</p>
-                            <div class=" col-sm-4">
-                                <select class="form-control">
-                                    <option>Today</option>
-                                    <option>This week</option>
-                                    <option>This month</option>
-                                    <option>This year</option>
-                                </select>
-                            </div>
-                        </div>
-                    </form>
-                    <h4>Used Storage</h4>
-                    <p>120GB/ 700GB</p>
-                    <div class="row">
-                        <div class="col-md-7">
-                            <table>
-                                <tr>
-                                    <td>
-                                        <input type="text" class="knob" value="120" data-width="150" data-height="150" data-skin="tron" data-thickness=.2 data-fgColor="#3c8dbc">
-                                    </td>
-                                    <td>
-                                        <p><b>Database: 40GB</b></p>
-                                        <p><b>File: 80GB</b></p>
-                                    </td>
-                                </tr>
-                            </table>
-                        </div>
-                        <div class="col-md-5">
-                            <h4>Companies & Users</h4>
-                            <h5>509 companies</h5>
-                            <ul>
-                                <li>300 free</li>
-                                <li>209 fee</li>
-                            </ul>
-                            <b>Total: 1390 users</b>
-                        </div>
-                    </div>
-                </div><!-- /.box-body -->
-                <div class="box-footer">
-                    Footer
-                </div><!-- /.box-footer-->
-            </div><!-- /.box -->
-            <div class="box">
-                <div class="box-header with-border">
-                    <h3 class="box-title">Monthly Recap Report</h3>
-                    <div class="box-tools pull-right">
-                        <button class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse"><i class="fa fa-minus"></i></button>
-                        <button class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove"><i class="fa fa-times"></i></button>
-                    </div>
-                </div>
-                <div class="box-body">
-                    <form role="form" class="form-horizontal">
-                        <div class="form-group">
-                            <p class="col-sm-8 help-block text-right">(Contains of today, week, month, year)</p>
-                            <div class=" col-sm-4">
-                                <select class="form-control">
-                                    <option>Today</option>
-                                    <option>This week</option>
-                                    <option>This month</option>
-                                    <option>This year</option>
-                                </select>
-                            </div>
-                        </div>
-                    </form>
-                    <canvas id="barChart"></canvas>
-                </div><!-- /.box-body -->
-            </div><!-- /.box -->
-
-            <div class="box">
-                <div class="box-header with-border">
-                    <h3 class="box-title">Top saling</h3>
-                    <div class="box-tools pull-right">
-                        <button class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse"><i class="fa fa-minus"></i></button>
-                        <button class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove"><i class="fa fa-times"></i></button>
-                    </div>
-                </div>
-                <div class="box-body">
-                    <form role="form" class="form-horizontal">
-                        <div class="form-group">
-                            <p class="col-sm-8 help-block text-right">(Contains of today, week, month, year)</p>
-                            <div class=" col-sm-4">
-                                <select class="form-control">
-                                    <option>Today</option>
-                                    <option>This week</option>
-                                    <option>This month</option>
-                                    <option>This year</option>
-                                </select>
-                            </div>
-                        </div>
-                    </form>
-                    <canvas id="horizontal-chart"></canvas>
-                </div><!-- /.box-body -->
-            </div><!-- /.box -->
-            <div class="box">
-                <div class="box-header with-border">
-                    <h3 class="box-title">Config system</h3>
-                    <div class="box-tools pull-right">
-                        <button class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse"><i class="fa fa-minus"></i></button>
-                        <button class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove"><i class="fa fa-times"></i></button>
-                    </div>
-                </div>
-                <div class="box-body">
-                    <table class="table table-striped">
-                        <tr>
-                            <th>Name:</th>
-                            <td>Centeroffice</td>
-                        </tr>
-                        <tr>
-                            <th>Maintain mode:</th>
-                            <td>FALSE</td>
-                        </tr>
-                    </table>
-                </div><!-- /.box-body -->
-            </div><!-- /.box -->
-
-        </div>
-
-        <div class="col-md-4 padding-left-md-0">
-            <div class="box box-widget">
-                <div class="box-header with-border">
-                    <h3 class="box-title">New company</h3>
-
-                    <div class="box-tools pull-right">
-                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                        </button>
-                    </div>
-                    <!-- /.box-tools -->
-                </div>
-                <!-- /.box-header -->
-                <div class="box-body">
-                    <ul class="products-list product-list-in-box">
-                        <li class="item">
-                            <div class="item-info">
-                                <h4 class="item-title">iCare company</h4>
-                                <span class="item-description">
-                                    register D package
-                                </span>
-                                <span class="item-price"> 3,000,000 VND</span>
-                                <small>5 hours 30 minutes before</small>
-                            </div>
-                        </li>
-                        <!-- /.item -->
-                        <li class="item">
-                            <div class="item-info">
-                                <h4 class="item-title">iCare company</h4>
-                                <span class="item-description">
-                                    register D package
-                                </span>
-                                <span class="item-price"> 3,000,000 VND</span>
-                                <small>5 hours 30 minutes before</small>
-                            </div>
-                        </li>
-                        <!-- /.item -->
-                        <li class="item">
-                            <div class="item-info">
-                                <h4 class="item-title">iCare company</h4>
-                                <span class="item-description">
-                                    register D package
-                                </span>
-                                <span class="item-price"> 3,000,000 VND</span>
-                                <small>5 hours 30 minutes before</small>
-                            </div>
-                        </li>
-                        <!-- /.item -->
-                    </ul>
-                </div>
-                <div class="box-footer text-center">
-                    <a href="javascript:void(0)" class="uppercase">View All</a>
+            <div class='content-timeline'>
+                <h3 class="timeline-header">Standard package  - 12 months</h3>
+                <div class="timeline-body">
+                    Standard package  - 12 months
                 </div>
             </div>
-            <div class="box box-widget">
-                <div class="box-header with-border">
-                    <h3 class="box-title">Duedate company</h3>
-
-                    <div class="box-tools pull-right">
-                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                        </button>
-                    </div>
-                    <!-- /.box-tools -->
-                </div>
-                <!-- /.box-header -->
-                <div class="box-body">
-                    <ul class="products-list product-list-in-box">
-                        <li class="item">
-                            <div class="item-info">
-                                <h4 class="item-title">iCare company</h4>
-                                <span class="item-description">
-                                    register D package
-                                </span>
-                                <span class="item-price"> 3,000,000 VND</span>
-                                <small>5 hours 30 minutes before</small>
-                            </div>
-                        </li>
-                        <!-- /.item -->
-                        <li class="item">
-                            <div class="item-info">
-                                <h4 class="item-title">iCare company</h4>
-                                <span class="item-description">
-                                    register D package
-                                </span>
-                                <span class="item-price"> 3,000,000 VND</span>
-                                <small>5 hours 30 minutes before</small>
-                            </div>
-                        </li>
-                        <!-- /.item -->
-                        <li class="item">
-                            <div class="item-info">
-                                <h4 class="item-title">iCare company</h4>
-                                <span class="item-description">
-                                    register D package
-                                </span>
-                                <span class="item-price"> 3,000,000 VND</span>
-                                <small>5 hours 30 minutes before</small>
-                            </div>
-                        </li>
-                        <!-- /.item -->
-                    </ul>
-                </div>
-                <div class="box-footer text-center">
-                    <a href="javascript:void(0)" class="uppercase">View All</a>
-                </div>
+        </li>
+        <li class='work'>
+            <div class="relative">
+                <span class='date'>30/8/2016</span>
+                <span class='circle'></span>
             </div>
-        </div>
-    </div>
+            <div class='content-timeline'>
+                <h3 class="timeline-header">Standard package  - 24 months</h3>
+            </div>
+        </li>
+        <li class='work'>
+            <div class="relative">
+                <span class='date'>30/9/2018</span>
+                <span class='circle'></span>
+            </div>
+            <div class='content-timeline'>
+                <h3 class="timeline-header">Standard package  - 36 months</h3>
+            </div>
+        </li>
+
+    </ul>
 </section>
