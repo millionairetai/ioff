451753748c231561335711a6552c561f97f21a6f
Tran Van Tai <millionairetai@gmail.com>
2017-03-22 14:26:17 +0700

    # apiKey & secret key

--- common/config/params.php
+++ common/config/params.php
@@ -4,9 +4,14 @@ return [
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
     'PathUpload' => dirname(dirname(dirname(__FILE__)))."/member/web/upload",
-    'frontendDomain' => 'http://iofficez.dev',
-    'backendDomain' => 'htttp://backend.iofficez.dev',
-    'companyDomain' => 'http://company.iofficez.dev',
+//    'frontendDomain' => 'http://iofficez.dev',
+//    'backendDomain' => 'htttp://backend.iofficez.dev',
+//    'companyDomain' => 'http://company.iofficez.dev',
+    
+    'frontendDomain' => 'http://iofficez.com',
+    'backendDomain' => 'htttp://backend.iofficez.com',
+    'companyDomain' => 'http://company.iofficez.com',
+    
     'number_credit_card' => 1039390902,
     'support_email' => 'support@iofficez.com',
     'hot_line_number' => '1900283',
@@ -17,5 +22,9 @@ return [
     'phone_from' => '083303239009',
     'email_from' => 'support@iofficez.com',
     'tax_percent' => 0,
-    'total_disk' => 1000000000
+    'total_disk' => 1000000000,
+    
+    //Sms config
+    'apiKey' => '02B1D60DA2C2B62DD90116303D46C1', 
+    'secretKey' => '68EEE103A7FDD49EE57777312026A4'
 ];
